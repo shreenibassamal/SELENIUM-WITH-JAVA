d226e16ff2e316cf1460d3ede52f453a003c83e3
Shreenibas Samal <shreenibassamal@gmail.com>
2024-11-24 09:38:41 +0530

    assignment 01 validate title of facebook

---
+++ 01_basics/Assignment_01.java
@@ -0,0 +1,31 @@
+package basic01;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+ * assignment 01
+ * **********************************************
+ * 1)Launch Browser(chrome).
+ * 2)Open URL  https://www.facebook.com/
+ * 3)validate title should be "Facebook – log in or sign up"
+ * 4)close page
+ */
+
+public class Assignment_01 {
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		System.out.println("this is the title of the page"+driver.getTitle());
+		String actualTitle = driver.getTitle();
+		if (actualTitle.equals("Facebook – log in or sign up")) {
+			System.out.println("Test passed");
+			
+		}
+		else {
+			System.out.println("Test failed");
+			
+		}
+		
+	}
+
+}
